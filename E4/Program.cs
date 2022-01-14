6bfb11971aaa94e3a7be767fcd61b9819ec9c311
Ilfat <ilfat95@mail.ru>
2022-01-14 18:00:30 +0500

    del дополнительной функции

--- E4/Program.cs
+++ E4/Program.cs
@@ -29,49 +29,8 @@ namespace E4
                 Console.WriteLine($"Количество маршрутов: {countWay}");
 
 
-                drawWays(in clients, countWay);
             }
             while (true);
-        }
-        static void drawWays(in List<int> clients, int countWay)
-        {
-            int i, j;
-            DataTable dataTable = new DataTable();
-            for (j = 0; j < clients.Count; j++)
-            {
-                dataTable.Columns.Add(new DataColumn(j.ToString(), Type.GetType("System.Int32")));
-            }
-
-            for (i = 0; i < countWay; i++)
-            {
-                dataTable.Rows.Add(dataTable.NewRow());
-            }
-
-            j = 0;
-            foreach (int client in clients)
-            {
-                i = 0;
-                for (i = i; i < dataTable.Rows.Count; i++)
-
-                {
-                    dataTable.Rows[i][j] = client;
-                    j++;
-                    if (j == clients.Count) j -= clients.Count;
-                }
-                j++;
-            }
-
-            for (i = 0; i < countWay; i++)
-            {
-                for (j = 0; j < clients.Count; j++)
-                {
-                    Console.Write(dataTable.Rows[i][j]);
-                }
-                Console.Write("\n");
-
-            }
-
-
         }
 
     }
