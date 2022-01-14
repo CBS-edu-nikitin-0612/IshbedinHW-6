1ef6e24680afaf338526ddce9482f9160b039d52
Ilfat <ilfat95@mail.ru>
2022-01-14 20:40:35 +0500

    add task 6

---
+++ Task6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5, b = 20, i, j;
+            for (i = 0; i < a; i++)
+            {
+                for (j = 0; j < b; j++) Console.Write("*");
+                Console.Write("\n");
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
